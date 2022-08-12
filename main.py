53e0aee46c734d584f8a7465c790bc29b5e0591d
ndyt <ntomlinson@gmail.com>
2022-08-11 21:20:22 -0400

    main function set up

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-# John has a cattle farm which consists of a few pens. Each pen houses some cows. 
-# The cows in each pen are of the same breed. Each breed of cow eats only one type of feed, 
-# yields a certain amount of milk, and generates a certain level of greenhouse gases.
+"""
+John has a cattle farm which consists of a few pens. Each pen houses some cows. 
+The cows in each pen are of the same breed. Each breed of cow eats only one type of feed, 
+yields a certain amount of milk, and generates a certain level of greenhouse gases.
 
-# John has asked you to help him simulate his farm so he can foresee the consequences of his 
-# decisions. Develop a code based on OOP to simulate this cattle farm, and run a few scenarios 
-# to showcase how it works. Some example features of this simulation are: 
+John has asked you to help him simulate his farm so he can foresee the consequences of his 
+decisions. Develop a code based on OOP to simulate this cattle farm, and run a few scenarios 
+to showcase how it works. Some example features of this simulation are: 
 
-# Pen allocation: What breed is allocated to this pen? How many cows are there?
+Pen allocation: What breed is allocated to this pen? How many cows are there?
 
-# Milk yield calculation: How much milk should we expect from each pen? Can you 
-# make a time series?
+Milk yield calculation: How much milk should we expect from each pen? Can you 
+make a time series?
 
-# Greenhouse gas emissions: Overall, how much greenhouse gases does this scenario generate?
+Greenhouse gas emissions: Overall, how much greenhouse gases does this scenario generate?
 
-# estimates from online searches: 
-# 1 dairy cow produces avg of 7.5 gallons milk/day
-# 1 liter of milk produces 1.39 kg of CO2 equivalents to the atmosphere
-# 1 liter = 0.264172 gallons
-# 7.5 gallons of milk produces 2.7539931 kg of CO2 per day
+Estimates from online searches: 
+1 dairy cow produces avg of 7.5 gallons milk/day 
+https://www.ciwf.com/farmed-animals/cows/dairy-cows/#:~:text=Milk%20production%20per%20cow%20has,gallon%20of%20milk%20per%20day
+1 liter of milk produces 1.39 kg of CO2 equivalents to the atmosphere
+https://blogs.nicholas.duke.edu/citizenscientist/how-green-is-your-milk/
+1 liter = 0.264172 gallons
+7.5 gallons of milk produces ~2.7539931 kg of CO2 per day
 
+Cost estimation: Based on the type and amount of the consumed feeds, how much does 
+it cost John to implement this scenario in his farm?
+"""
 
-# Cost estimation: Based on the type and amount of the consumed feeds, how much does 
-# it cost John to implement this scenario in his farm?
+from view import *
 
+from db.farm_database import SQLFarmDB
+
+from view.view import View
+from view_model import ViewModel
 
 
 def main():
 
-    # initialize db (if using)
+    farm_db = SQLFarmDB()
 
-    # initialize viewmodel instance
+    farm_view_model = ViewModel(farm_db)
 
-    # initialize view instance
+    farm_view = View(farm_view_model)
 
-    # start data intake from farmer
-
+    farm_view.get_new_pens()
 
 
 if __name__ == '__main__':
--- view/view.py
+++ view/view.py
@@ -10,8 +10,8 @@ class View:
 
     def get_new_pens(self):
 
-        header('Add information about a new pen')
-
+        header('Welcome to the RuFas-beta model, John!\nAdd information about a new pen')
+        print('For this first pen:\n')
         while True:
             pen = self.get_new_pen_data()
             if not pen:
@@ -19,20 +19,20 @@ class View:
     
     def get_new_pen_data(self):
 
-        print('For this pen:\n')
+        print('For the next pen:\n')
         breed = input('Enter the name of the breed of cow or press "enter" to quit: ')
         if not breed:
             return
         
-        number_of_cows = input_positive_float(f'How many total {breed}s does it hold? ')
+        number_of_cows = input_positive_float(f'How many total {breed} does the pen hold? ')
         feed_type = input('What type of feed are you using? ')
-        amount_of_feed_per_cow = input_positive_float('How much feed does one cow eat per day? ')
-        milk_yield = input_positive_float('How much milk on average does one cow produce per day? ')
-
+        amount_of_feed_per_cow = input_positive_float('How many pounds of feed does one cow eat per day? ')
+        milk_yield = input_positive_float('How much milk (in gallons) does one cow produce per day? ')
+        print()
         pen = Pen(breed, number_of_cows, feed_type, amount_of_feed_per_cow, milk_yield)
 
         try:
-            self.view_model.add(pen)
+            self.view_model.insert(pen)
             return pen
         except FarmError as e:
             print(str(e))
